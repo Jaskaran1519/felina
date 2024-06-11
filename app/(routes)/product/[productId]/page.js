cfb8b15a76604fa5bd2bbdcd655269a67aec7e4b
Jaskaran1519 <jaskaran1519@gmail.com>
2024-06-11 15:07:24 +0530

    okok

--- app/(routes)/product/[productId]/page.js
+++ app/(routes)/product/[productId]/page.js
@@ -4,6 +4,8 @@ import { database } from "../../../firebaseConfig";
 import { get, ref } from "firebase/database";
 import Image from "next/image";
 import { Context } from "../../../../context/page";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const page = ({ params }) => {
   const id = params.productId;
@@ -42,73 +44,102 @@ const page = ({ params }) => {
   const { handleAddtocart, handleAddtoWishlist } = useContext(Context);
 
   return (
-    product && (
-      <div className="w-[85%] mx-auto mt-[10vh] h-auto md:min-h-[50vh] xl:min-h-screen">
-        <div className="flex gap-8 flex-wrap">
-          <div className="w-full h-[40vh] lg:w-[45vw] xl:h-[70vh]  relative">
-            <Image
-              className="w-full  object-cover rounded-md"
-              src={product.image}
-              layout="fill"
-              alt=".."
-            />
-          </div>
-          <div className=" w-full lg:w-[35vw] h-auto">
-            <h1 className="text-[2.2rem] leading-10 text-herofont font-semibold text-gray-800">
-              {product.name}
-            </h1>
-            <p className="mt-3 text-gray-600 font-medium">
-              {product.description}
-            </p>
-
-            <div className="flex justify-between items-center mt-[5vh]">
-              <div className="flex gap-3">
-                <p className="line-through text-[1.5rem] font-bold text-gray-500 ">
-                  ₹{product.fake}
-                </p>
-                <p className="text-[1.5rem] font-bold text-gray-700">
-                  ₹{product.price}
-                </p>
+    <div className="w-[85%] mx-auto mt-[10vh] h-auto md:min-h-[50vh] xl:min-h-screen">
+      <div className="flex gap-8 flex-wrap">
+        {product ? (
+          <>
+            <div className="w-full h-[40vh] lg:w-[45vw] xl:h-[70vh] relative">
+              <Image
+                className="w-full object-cover rounded-md"
+                src={product.image}
+                layout="fill"
+                alt=".."
+              />
+            </div>
+            <div className="w-full lg:w-[35vw] h-auto">
+              <h1 className="text-[2.2rem] leading-10 text-herofont font-semibold text-gray-800">
+                {product.name}
+              </h1>
+              <p className="mt-3 text-gray-600 font-medium">
+                {product.description}
+              </p>
+              <div className="flex justify-between items-center mt-[5vh]">
+                <div className="flex gap-3">
+                  <p className="line-through text-[1.5rem] font-bold text-gray-500">
+                    ₹{product.fake}
+                  </p>
+                  <p className="text-[1.5rem] font-bold text-gray-700">
+                    ₹{product.price}
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="font-bold">4.3</p>
+                  <Image src="/star.svg" width={15} height={15} alt="/" />
+                  <p className="pl-3">{`(18 reviews)`}</p>
+                </div>
               </div>
-              <div className="flex ">
-                <p className="font-bold ">4.3</p>
-                <Image src="/star.svg" width={15} height={15} alt="/" />
-                <p className="pl-3">{`(18 reviews)`}</p>
+              <div className="w-[60%] mx-auto mt-4">
+                <button
+                  onClick={() => {
+                    handleAddtocart(product);
+                    setIsAdding(true);
+                  }}
+                  className="w-full my-2 bg-gray-200 rounded-full p-3 text-[1.25rem] hover:bg-gray-300 cursor-pointer font-semibold"
+                >
+                  {isAdding ? (
+                    <Image
+                      className="mx-auto"
+                      src="/addtocart.gif"
+                      width={30}
+                      height={30}
+                      alt="Adding to cart"
+                    />
+                  ) : (
+                    "Add to cart"
+                  )}
+                </button>
+                <button
+                  className="w-full my-2 bg-gray-200 rounded-full p-3 text-[1.25rem] hover:bg-gray-300 cursor-pointer font-semibold"
+                  onClick={() => {
+                    handleAddtoWishlist(product);
+                  }}
+                >
+                  Add to wishlist
+                </button>
               </div>
             </div>
-            <div className="w-[60%] mx-auto mt-4">
-              <button
-                onClick={() => {
-                  handleAddtocart(product);
-                  setIsAdding(true);
-                }}
-                className="w-full my-2 bg-gray-200  rounded-full p-3 text-[1.25rem] hover:bg-gray-300 cursor-pointer  font-semibold"
-              >
-                {isAdding ? (
-                  <Image
-                    className="mx-auto"
-                    src="/addtocart.gif"
-                    width={30}
-                    height={30}
-                    alt="Adding to cart"
-                  />
-                ) : (
-                  "Add to cart"
-                )}
-              </button>
-              <button
-                className="w-full my-2 bg-gray-200 rounded-full p-3 text-[1.25rem] hover:bg-gray-300 cursor-pointer  font-semibold"
-                onClick={() => {
-                  handleAddtoWishlist(product);
-                }}
-              >
-                Add to wishlist
-              </button>
+          </>
+        ) : (
+          <>
+            <div className="w-full h-[40vh] lg:w-[45vw] xl:h-[70vh] relative">
+              <Skeleton height="100%" />
             </div>
-          </div>
-        </div>
+            <div className="w-full lg:w-[35vw] h-auto">
+              <Skeleton count={1} height={35} width="60%" />
+              <Skeleton
+                count={3}
+                height={15}
+                width="80%"
+                style={{ marginTop: "10px" }}
+              />
+              <Skeleton height={25} width="30%" style={{ marginTop: "20px" }} />
+              <Skeleton height={25} width="50%" style={{ marginTop: "10px" }} />
+              <Skeleton height={25} width="40%" style={{ marginTop: "10px" }} />
+              <Skeleton
+                height={40}
+                width="100%"
+                style={{ marginTop: "20px" }}
+              />
+              <Skeleton
+                height={40}
+                width="100%"
+                style={{ marginTop: "10px" }}
+              />
+            </div>
+          </>
+        )}
       </div>
-    )
+    </div>
   );
 };
 
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,15 +1,24 @@
 "use client";
 import { Heart, Info, ShoppingCartIcon } from "lucide-react";
+import { Context } from "../../context/page";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 const Header = () => {
+  const { cartItems } = useContext(Context);
   return (
     <div className="flex gap-[3vw] items-center text-[3vh] mt-4 ">
       <Link href="/wishlist">
         <Heart />
       </Link>
       <Link href="/cart">
-        <ShoppingCartIcon />
+        <div className="relative">
+          <ShoppingCartIcon />
+          {cartItems && cartItems.length > 0 ? (
+            <span className="w-4 h-4 bg-orange-800 text-[0.5rem] rounded-full absolute -top-2 -right-2 text-white flex justify-center items-center -z-10">
+              {cartItems.length}
+            </span>
+          ) : null}
+        </div>
       </Link>
       <Link href="/product">
         <button className="text-[2vh] px-3 py-1.5 bg-orange-900 text-white font-bold rounded">
